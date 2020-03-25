7f7de70ae4f2f3215f8b5282b2d5313430c02a97
ngel rojas p <aomarrojasp@gmail.com>
2020-03-25 16:24:33 -0300

    add drf3, drf_yasg

--- app/api/settings.py
+++ app/api/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+from corsheaders.defaults import default_methods
+from corsheaders.defaults import default_headers
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+X_FRAME_OPTIONS = 'Deny'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -38,11 +39,15 @@ APP_LOCAL = [
     'django.contrib.staticfiles',
 ]
 
-APP_THRIDPARTY = []
+APP_THRIDPARTY = [
+    'rest_framework',
+    'drf_yasg',
+    'corsheaders',
+]
 
-APP_DEV = []
+APP_API = []
 
-INSTALLED_APPS = APP_LOCAL + APP_THRIDPARTY + APP_DEV
+INSTALLED_APPS = APP_LOCAL + APP_THRIDPARTY + APP_API
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -89,6 +94,14 @@ DATABASES = {
     }
 }
 
+# dictionary REST_FRAMEWORK
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -128,3 +141,27 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# CORS AND STUFF
+CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = list(default_methods)
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'http://localhost:3000',
+)
+CSRF_COOKIE_NAME = 'csrftoken'
+
+CSRF_TRUSTED_ORIGINS = (
+    'localhost:3000',
+)
+
+CORS_ALLOW_HEADERS = default_headers + (
+    'x-xsrf-token',
+    'HTTP_X_XSRF_TOKEN',
+    "X-ACCESS_TOKEN",
+)
