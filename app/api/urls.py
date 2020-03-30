eb7c471dbcba2b5d2e856d68b164caf89e48a70a
ngel rojas p <aomarrojasp@gmail.com>
2020-03-29 22:08:08 -0300

    url update

--- app/api/urls.py
+++ app/api/urls.py
@@ -20,16 +20,16 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-        openapi.Info(
-            title="MY API",
-            default_version='v1',
-            description="my api example",
-            terms_of_service="https://ngelrojasp.com/policies/terms/",
-            contact=openapi.Contact(email="me@ngelrojasp.com"),
-            license=openapi.License(name="BSD License"),
-        ),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="MY API",
+        default_version='v1',
+        description="my api example",
+        terms_of_service="https://ngelrojasp.com/policies/terms/",
+        contact=openapi.Contact(email="me@ngelrojasp.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
