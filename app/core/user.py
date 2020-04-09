5fd84e19804abfbb75ab94f36f166572c68b28fe
ngel rojas p <aomarrojasp@gmail.com>
2020-04-09 18:24:52 -0300

    add migration table user to module users

--- app/core/user.py
+++ app/core/user.py
@@ -1,25 +1,23 @@
-import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 
-from django.conf import settings
-
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """custom common user"""
+        """creates and saves a new user"""
         if not email:
-            raise ValueError('User must have an email address.')
+            raise ValueError('Users must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, password):
-        """custom super user"""
+        """creates and saves new superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -28,11 +26,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """custom user"""
+    """custom user model that supposrts using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_activate = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
