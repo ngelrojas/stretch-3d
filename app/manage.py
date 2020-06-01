6a25809267f38ee0e5bc3de0df3ac6fa6a7a7e58
ngel rojas p <aomarrojasp@gmail.com>
2020-05-31 23:27:33 -0300

    remove setting environment

--- app/manage.py
+++ app/manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.development")
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.production")
     try:
