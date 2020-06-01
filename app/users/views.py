a77b2fb7dfb690820f98a27ba4b5b537b89fa24e
ngel rojas <aomarrojasp@gmail.com>
2020-06-01 18:27:38 -0300

    add resouces for users models

--- app/users/views.py
+++ app/users/views.py
@@ -1,1 +1,20 @@
-# from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from core.user import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """create new user """
+
+    serializer_class = UserSerializer
+    permission_class = AllowAny
+    queryset = User.objects.all()
+
+    def get(self, request):
+        """list all users"""
+        queryset = User.objects.all()
+        serializer = self.serializer_class(queryset)
+        return Response({"data": serializer.data})
