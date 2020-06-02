bda5c501ce772f6d9adcc4ef1b10632e1626741e
ngel rojas <aomarrojasp@gmail.com>
2020-06-02 18:15:33 -0300

    add url create and retrieve in different veiws

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import UserViewSet
+from .views import CreateUserView, UserViewSet
 
 app_name = "user"
 
-urlpatterns = [path("user", UserViewSet.as_view({"get": "list"}), name="create")]
+urlpatterns = [
+    path("user", CreateUserView.as_view(), name="create"),
+    path(
+        "user/<int:pk>", UserViewSet.as_view({"get": "retrieve"}), name="user-details",
+    ),
+]
