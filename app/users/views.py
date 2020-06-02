acc8223b97ef535dc62003e3fdc0357c7e60d702
ngel rojas <aomarrojasp@gmail.com>
2020-06-02 18:16:47 -0300

    add views create and retrieve

--- app/users/views.py
+++ app/users/views.py
@@ -2,19 +2,27 @@ from rest_framework import generics, viewsets
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from core.user import User
+from core.query_core.queryUser import QueryUser
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class CreateUserView(generics.CreateAPIView):
     """create new user """
 
     serializer_class = UserSerializer
     permission_class = AllowAny
-    queryset = User.objects.all()
 
-    def get(self, request):
-        """list all users"""
-        queryset = User.objects.all()
-        serializer = self.serializer_class(queryset)
-        return Response({"data": serializer.data})
+
+class UserViewSet(viewsets.ModelViewSet):
+    """all about user update, list and retrieve"""
+
+    serializer_class = UserSerializer
+    queryset = QueryUser.all_users()
+
+    def retrieve(self, request, pk):
+        """
+            retrieve current user
+        """
+        current_user = QueryUser.current_user(self, request)
+        serializer = self.serializer_class(current_user)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
