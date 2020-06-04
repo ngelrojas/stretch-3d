39255c88c49d3d9adf87332f9b45d4f0629645ec
ngel rojas <aomarrojasp@gmail.com>
2020-06-04 15:07:29 -0300

    add new pytest

--- app/users/tests/test_user.py
+++ app/users/tests/test_user.py
@@ -1,14 +1,19 @@
 import pytest
 from django.test import TestCase
+from django.test.client import Client
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 URL_USER_DETAILS = reverse("user:user-details", (), {"pk": 2})
+URL_CREATE_USER = reverse("user:create")
 
 
 class UserManagerTests(TestCase):
     """test manager user"""
+
+    def setUp(self):
+        self.client = Client()
 
     def test_create_user(self):
         User = get_user_model()
@@ -34,9 +39,13 @@ class UserManagerTests(TestCase):
         assert User.objects.count() == 1
 
     @pytest.mark.django_db
-    def test_view_pytest(self):
-        User = get_user_model()
-        user = User.objects.create_user("me@ngelrojasp.com", "me123456")
-        url = reverse("user:user-details", (), {"pk", user.pk})
-        response = self.client.get(URL_USER_DETAILS)
-        assert response.status_code == 200
+    def test_create_user_url_pytest(self):
+        """create user throught end-point"""
+        payload = {
+            "fist_name": "ngel",
+            "last_name": "rojas",
+            "email": "me@ngelrojasp.com",
+            "password": "me123456",
+        }
+        response = self.client.post(URL_CREATE_USER, payload)
+        assert response.status_code == 201
