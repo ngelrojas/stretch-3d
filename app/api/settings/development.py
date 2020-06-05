28a8e6b3e45bcf283ce3f93487d3d9ba79c6014f
ngel rojas <aomarrojasp@gmail.com>
2020-06-05 18:28:17 -0300

    add xlsx to development

--- app/api/settings/development.py
+++ app/api/settings/development.py
@@ -100,9 +100,12 @@ DATABASES = {
 
 # dictionary REST_FRAMEWORK
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"]
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "drf_renderer_xlsx.renderers.XLSXRenderer",
+    ),
 }
 
 # Password validation
