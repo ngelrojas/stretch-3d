2ed0f84f7477aca3784270ba619ac8656e0bf80b
ngel rojas <aomarrojasp@gmail.com>
2020-06-05 18:30:16 -0300

    add exec end-point

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import CreateUserView, UserViewSet
+from .excel import ExcelViewSet
 
 app_name = "user"
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path(
         "user/<int:pk>", UserViewSet.as_view({"get": "retrieve"}), name="user-details",
     ),
+    path("excel", ExcelViewSet.as_view({"get": "list"}), name="user-excel"),
 ]
