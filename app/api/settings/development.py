5344aa640ffb1c09c77e2efdd5b529caead69229
ngel rojas <aomarrojasp@gmail.com>
2020-06-17 11:10:32 -0300

    add package django-grpc

--- app/api/settings/development.py
+++ app/api/settings/development.py
@@ -104,7 +104,6 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
-        "drf_renderer_xlsx.renderers.XLSXRenderer",
     ),
 }
 
