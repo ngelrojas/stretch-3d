647afe40a26163a81950833146eb4bf19a94e98b
ngel rojas <aomarrojasp@gmail.com>
2020-06-17 11:12:06 -0300

    remove excel module

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import CreateUserView, UserViewSet
-from .excel import ExcelViewSet
 
 app_name = "user"
 
@@ -9,5 +8,4 @@ urlpatterns = [
     path(
         "user/<int:pk>", UserViewSet.as_view({"get": "retrieve"}), name="user-details",
     ),
-    path("excel", ExcelViewSet.as_view({"get": "list"}), name="user-excel"),
 ]
